60f909f75b30f3e59fa80e340b7b1d6fa2792260
p-zielinski <82354717+p-zielinski@users.noreply.github.com>
2022-07-31 21:09:51 +0200

    in progress

--- utils/connection.ts
+++ utils/connection.ts
@@ -1,22 +1,23 @@
 //IMPORT MONGOOSE
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 import UserSchema from "./models/userSchema";
+import { UserRoles } from "./enums/userRoles";
 
 // CONNECTING TO MONGOOSE (Get Database Url from .env.local)
-const { DATABASE_URL } = process.env
+const { DATABASE_URL } = process.env;
 
 // connection function
 export const connect = async () => {
-    const conn = await mongoose
-        .connect(DATABASE_URL as string).then(data => {
-            console.log("Mongoose Connection Established")
-            return data
-        })
-        .catch(err => console.log(err))
-    
+  const conn = await mongoose
+    .connect(DATABASE_URL as string)
+    .then((data) => {
+      console.log("Mongoose Connection Established");
+      return data;
+    })
+    .catch((err) => console.log(err));
 
-    //Models
-    const User = mongoose.models.User || mongoose.model("User", UserSchema)
+  //Models
+  const User = mongoose.models.User || mongoose.model("User", UserSchema);
 
-    return { conn, User }
-}
+  return { conn, User };
+};
--- utils/models/userSchema.ts
+++ utils/models/userSchema.ts
@@ -1,27 +1,29 @@
 import mongoose from "mongoose";
-import {UserRoles} from "../enums/userRoles";
+import { UserRoles } from "../enums/userRoles";
 
 const UserSchema = new mongoose.Schema({
-    password: {
-        type: String,
-        required: true,
-        unique: false
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: false
-    },
-    email_confirmed:{
-        type: Boolean,
-        required: false,
-        default: false
-    },
-    role: {
-        type:String,
-        required: false,
-        default: UserRoles.USER
-    }
-})
+  password: {
+    type: String,
+    required: true,
+    unique: false,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email_confirmed: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
+  role: {
+    type: String,
+    enum: Object.values(UserRoles),
+    required: false,
+    default: UserRoles.USER,
+  },
+  created_at: { type: Date, default: Date.now },
+});
 
-export default UserSchema
+export default UserSchema;
